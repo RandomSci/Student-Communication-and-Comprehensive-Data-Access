b592a4884bf4193e2c98466c44333af5aad44019
Selwyn Jayme <143252444+RandomSci@users.noreply.github.com>
2024-08-03 22:48:22 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def Home():
+    return render_template("Registration.html")
+
+@app.route("/Registration_links", methods = ["POST", "GET"])
+def Registration():
+    link = request.form.get("link")
+    if request.method == "POST":
+        if link == "Teacher":
+            return render_template("ForTeacherRegistration.html")
+        elif link == "Student":
+            return render_template("ForStudentRegistration.html")
+    else:
+        return render_template("Registration.html")
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
